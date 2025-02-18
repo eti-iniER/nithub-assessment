5b5335690df02c9c7be516d4948b71ab180f7cec
Eti-ini Effiong-Robert <etinieffiongrobert@gmail.com>
2025-02-18 19:35:33 +0100

    Configure documentation and API routes

---
+++ api/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+from rest_framework.routers import DefaultRouter
+from api.views import ProductViewSet
+
+router = DefaultRouter()
+router.register("products", ProductViewSet, basename="products")
+
+urlpatterns = [
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "docs/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+]
+
+urlpatterns += router.urls
