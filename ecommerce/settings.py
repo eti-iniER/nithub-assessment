8482283061a614c67a64b6b3305e889092008ea1
Eti-ini Effiong-Robert <etinieffiongrobert@gmail.com>
2025-02-18 23:29:59 +0100

    Change default API permission to IsAuthenticated

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -58,7 +58,7 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+        "rest_framework.permissions.IsAuthenticated",
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
